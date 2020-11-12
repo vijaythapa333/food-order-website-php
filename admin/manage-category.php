d67f3ebeaf6a3a617331b3d867a790919be3835c
Vijay Thapa <vijaythapa333@gmail.com>
2020-11-12 22:23:30 +0545

    Add and Display Category Completed

--- admin/manage-category.php
+++ admin/manage-category.php
@@ -5,49 +5,117 @@
         <h1>Manage Category</h1>
 
         <br /><br />
+        <?php 
+        
+            if(isset($_SESSION['add']))
+            {
+                echo $_SESSION['add'];
+                unset($_SESSION['add']);
+            }
+        
+        ?>
+        <br><br>
 
                 <!-- Button to Add Admin -->
-                <a href="#" class="btn-primary">Add Category</a>
+                <a href="<?php echo SITEURL; ?>admin/add-category.php" class="btn-primary">Add Category</a>
 
                 <br /><br /><br />
 
                 <table class="tbl-full">
                     <tr>
                         <th>S.N.</th>
-                        <th>Full Name</th>
-                        <th>Username</th>
+                        <th>Title</th>
+                        <th>Image</th>
+                        <th>Featured</th>
+                        <th>Active</th>
                         <th>Actions</th>
                     </tr>
 
-                    <tr>
-                        <td>1. </td>
-                        <td>Vijay Thapa</td>
-                        <td>vijaythapa</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
+                    <?php 
 
-                    <tr>
-                        <td>2. </td>
-                        <td>Vijay Thapa</td>
-                        <td>vijaythapa</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
+                        //Query to Get all CAtegories from Database
+                        $sql = "SELECT * FROM tbl_category";
 
-                    <tr>
-                        <td>3. </td>
-                        <td>Vijay Thapa</td>
-                        <td>vijaythapa</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
+                        //Execute Query
+                        $res = mysqli_query($conn, $sql);
+
+                        //Count Rows
+                        $count = mysqli_num_rows($res);
+
+                        //Create Serial Number Variable and assign value as 1
+                        $sn=1;
+
+                        //Check whether we have data in database or not
+                        if($count>0)
+                        {
+                            //We have data in database
+                            //get the data and display
+                            while($row=mysqli_fetch_assoc($res))
+                            {
+                                $id = $row['id'];
+                                $title = $row['title'];
+                                $image_name = $row['image_name'];
+                                $featured = $row['featured'];
+                                $active = $row['active'];
+
+                                ?>
+
+                                    <tr>
+                                        <td><?php echo $sn++; ?>. </td>
+                                        <td><?php echo $title; ?></td>
+
+                                        <td>
+
+                                            <?php  
+                                                //Chcek whether image name is available or not
+                                                if($image_name!="")
+                                                {
+                                                    //Display the Image
+                                                    ?>
+                                                    
+                                                    <img src="<?php echo SITEURL; ?>images/category/<?php echo $image_name; ?>" width="100px" >
+                                                    
+                                                    <?php
+                                                }
+                                                else
+                                                {
+                                                    //DIsplay the MEssage
+                                                    echo "<div class='error'>Image not Added.</div>";
+                                                }
+                                            ?>
+
+                                        </td>
+
+                                        <td><?php echo $featured; ?></td>
+                                        <td><?php echo $active; ?></td>
+                                        <td>
+                                            <a href="#" class="btn-secondary">Update Category</a>
+                                            <a href="#" class="btn-danger">Delete Category</a>
+                                        </td>
+                                    </tr>
+
+                                <?php
+
+                            }
+                        }
+                        else
+                        {
+                            //WE do not have data
+                            //We'll display the message inside table
+                            ?>
+
+                            <tr>
+                                <td colspan="6"><div class="error">No Category Added.</div></td>
+                            </tr>
+
+                            <?php
+                        }
+                    
+                    ?>
+
+                    
+
+                    
                 </table>
     </div>
     
