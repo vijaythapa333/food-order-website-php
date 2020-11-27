ee5e2ccd8f85910fe31ac2da29c00410319a6d96
Vijay Thapa <vijaythapa333@gmail.com>
2020-11-27 21:15:05 +0545

    Delete and Update Food

---
+++ admin/delete-food.php
@@ -0,0 +1,71 @@
+<?php 
+    //Include COnstants Page
+    include('../config/constants.php');
+
+    //echo "Delete Food Page";
+
+    if(isset($_GET['id']) && isset($_GET['image_name'])) //Either use '&&' or 'AND'
+    {
+        //Process to Delete
+        //echo "Process to Delete";
+
+        //1.  Get ID and Image NAme
+        $id = $_GET['id'];
+        $image_name = $_GET['image_name'];
+
+        //2. Remove the Image if Available
+        //CHeck whether the image is available or not and Delete only if available
+        if($image_name != "")
+        {
+            // IT has image and need to remove from folder
+            //Get the Image Path
+            $path = "../images/food/".$image_name;
+
+            //REmove Image File from Folder
+            $remove = unlink($path);
+
+            //Check whether the image is removed or not
+            if($remove==false)
+            {
+                //Failed to Remove image
+                $_SESSION['upload'] = "<div class='error'>Failed to Remove Image File.</div>";
+                //REdirect to Manage Food
+                header('location:'.SITEURL.'admin/manage-food.php');
+                //Stop the Process of Deleting Food
+                die();
+            }
+
+        }
+
+        //3. Delete Food from Database
+        $sql = "DELETE FROM tbl_food WHERE id=$id";
+        //Execute the Query
+        $res = mysqli_query($conn, $sql);
+
+        //CHeck whether the query executed or not and set the session message respectively
+        //4. Redirect to Manage Food with Session Message
+        if($res==true)
+        {
+            //Food Deleted
+            $_SESSION['delete'] = "<div class='success'>Food Deleted Successfully.</div>";\
+            header('location:'.SITEURL.'admin/manage-food.php');
+        }
+        else
+        {
+            //Failed to Delete Food
+            $_SESSION['delete'] = "<div class='error'>Failed to Delete Food.</div>";\
+            header('location:'.SITEURL.'admin/manage-food.php');
+        }
+
+        
+
+    }
+    else
+    {
+        //Redirect to Manage Food Page
+        //echo "REdirect";
+        $_SESSION['unauthorize'] = "<div class='error'>Unauthorized Access.</div>";
+        header('location:'.SITEURL.'admin/manage-food.php');
+    }
+
+?>
--- admin/manage-food.php
+++ admin/manage-food.php
@@ -17,6 +17,30 @@
                         echo $_SESSION['add'];
                         unset($_SESSION['add']);
                     }
+
+                    if(isset($_SESSION['delete']))
+                    {
+                        echo $_SESSION['delete'];
+                        unset($_SESSION['delete']);
+                    }
+
+                    if(isset($_SESSION['upload']))
+                    {
+                        echo $_SESSION['upload'];
+                        unset($_SESSION['upload']);
+                    }
+
+                    if(isset($_SESSION['unauthorize']))
+                    {
+                        echo $_SESSION['unauthorize'];
+                        unset($_SESSION['unauthorize']);
+                    }
+
+                    if(isset($_SESSION['update']))
+                    {
+                        echo $_SESSION['update'];
+                        unset($_SESSION['update']);
+                    }
                 
                 ?>
 
@@ -83,8 +107,8 @@
                                     <td><?php echo $featured; ?></td>
                                     <td><?php echo $active; ?></td>
                                     <td>
-                                        <a href="#" class="btn-secondary">Update Food</a>
-                                        <a href="#" class="btn-danger">Delete Food</a>
+                                        <a href="<?php echo SITEURL; ?>admin/update-food.php?id=<?php echo $id; ?>" class="btn-secondary">Update Food</a>
+                                        <a href="<?php echo SITEURL; ?>admin/delete-food.php?id=<?php echo $id; ?>&image_name=<?php echo $image_name; ?>" class="btn-danger">Delete Food</a>
                                     </td>
                                 </tr>
 
