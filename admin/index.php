7c8b9aa5514a7b4f43ceec612f76e5282a0ac3c6
Vijay Thapa <vijaythapa333@gmail.com>
2020-11-30 21:54:00 +0545

    Summary Added on Admin Dashboard

--- admin/index.php
+++ admin/index.php
@@ -16,27 +16,74 @@
                 <br><br>
 
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+
+                    <?php 
+                        //Sql Query 
+                        $sql = "SELECT * FROM tbl_category";
+                        //Execute Query
+                        $res = mysqli_query($conn, $sql);
+                        //Count Rows
+                        $count = mysqli_num_rows($res);
+                    ?>
+
+                    <h1><?php echo $count; ?></h1>
                     <br />
                     Categories
                 </div>
 
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+
+                    <?php 
+                        //Sql Query 
+                        $sql2 = "SELECT * FROM tbl_food";
+                        //Execute Query
+                        $res2 = mysqli_query($conn, $sql2);
+                        //Count Rows
+                        $count2 = mysqli_num_rows($res2);
+                    ?>
+
+                    <h1><?php echo $count2; ?></h1>
                     <br />
-                    Categories
+                    Foods
                 </div>
 
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+                    
+                    <?php 
+                        //Sql Query 
+                        $sql3 = "SELECT * FROM tbl_order";
+                        //Execute Query
+                        $res3 = mysqli_query($conn, $sql3);
+                        //Count Rows
+                        $count3 = mysqli_num_rows($res3);
+                    ?>
+
+                    <h1><?php echo $count3; ?></h1>
                     <br />
-                    Categories
+                    Total Orders
                 </div>
 
                 <div class="col-4 text-center">
-                    <h1>5</h1>
+                    
+                    <?php 
+                        //Creat SQL Query to Get Total Revenue Generated
+                        //Aggregate Function in SQL
+                        $sql4 = "SELECT SUM(total) AS Total FROM tbl_order WHERE status='Delivered'";
+
+                        //Execute the Query
+                        $res4 = mysqli_query($conn, $sql4);
+
+                        //Get the VAlue
+                        $row4 = mysqli_fetch_assoc($res4);
+                        
+                        //GEt the Total REvenue
+                        $total_revenue = $row4['Total'];
+
+                    ?>
+
+                    <h1>$<?php echo $total_revenue; ?></h1>
                     <br />
-                    Categories
+                    Revenue Generated
                 </div>
 
                 <div class="clearfix"></div>
